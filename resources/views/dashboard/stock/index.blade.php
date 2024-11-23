73fd616854701c9040381312dca497a9479bd108
RaihanOprinande <opri.fahdi@gmail.com>
2024-11-23 19:06:19 +0700

    create sepatu size

--- resources/views/dashboard/stock/index.blade.php
+++ resources/views/dashboard/stock/index.blade.php
@@ -11,37 +11,37 @@
 
 <a href="/dashboard-stock/create" class="btn btn-primary mb-2">Add Stock</a>
 
-{{-- <table class="table table-bordered">
+<table class="table table-bordered">
     <tr>
         <th>No</th>
+        <th>Kode Sepatu</th>
         <th>Sepatu</th>
         <th>Size</th>
         <th>Quantity</th>
         <th>Aksi</th>
     </tr>
-    @foreach ($stocks as $stock)
-        <tr>
-            <td>{{ $stocks->firstItem() + $loop->index }}</td>
-            <td>{{ $stock->sepatu_id }}</td>
-            <td>{{ $stock->size_id }}</td>
-            <td>{{ $stock->quaintity }}</td>
-            <td class="text-nowrap">
-                <a href="/dashboard-stock/{{ $stock->id }}/edit" class="btn btn-warning btn-sm" title="Edit"><i class="bi bi-pencil-square"></i></a>
+    @foreach ($sepatusizes as $sepatu)
+        @foreach ($sepatu->sizes as $size )
+            <tr>
+                <td>{{ $sepatusizes->firstItem() + $loop->index }}</td>
+                <td>{{ $sepatu->kode_sepatu }}</td>
+                <td>{{ $sepatu->nama }}</td>
+                <td>{{ $size->size }}</td>
+                <td>{{ $size->pivot->quantity }}</td>   
+                <td class="text-nowrap">
+                    <a href="/dashboard-stock/{{ $sepatu->id }}/edit" class="btn btn-warning btn-sm" title="Edit"><i class="bi bi-pencil-square"></i></a>
 
-                <form action="/dashboard-stock/{{ $stock->id }}" method="post" class="d-inline">
-                    @method('DELETE')
-                    @csrf
-                    <button title="Delete" class="btn btn-danger btn-sm" onclick="return confirm('Apakah anda ingin menghapus pemasukan ini?')">
-                        <i class="bi bi-trash"></i>
-                    </button>
-                </form>
-            </td>
-        </tr>
+                    {{-- <form action="/dashboard-stock/{{ $sepatusizes->id }}" method="post" class="d-inline">
+                        @method('DELETE')
+                        @csrf
+                        <button title="Delete" class="btn btn-danger btn-sm" onclick="return confirm('Apakah anda ingin menghapus pemasukan ini?')">
+                            <i class="bi bi-trash"></i>
+                        </button>
+                    </form> --}}
+                </td>
+            </tr>
+        @endforeach
     @endforeach
-</table> --}}
-@foreach ($sepatus as $sepatu)
-    {{ $sepatu->pivot->nama }}
-    {{-- {{ $article->pivot->shortname }} --}}
-@endforeach
-{{ $stocks->links() }}
+</table>
+{{ $sepatusizes->links() }}
 @endsection
