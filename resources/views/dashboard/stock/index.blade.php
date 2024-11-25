e04f26d7a7aa98d26739e29db49165780f2a44d8
RaihanOprinande <opri.fahdi@gmail.com>
2024-11-25 18:36:55 +0700

    CRUD stocks

--- resources/views/dashboard/stock/index.blade.php
+++ resources/views/dashboard/stock/index.blade.php
@@ -20,27 +20,29 @@
         <th>Quantity</th>
         <th>Aksi</th>
     </tr>
-    @foreach ($sepatusizes as $sepatu)
-        @foreach ($sepatu->sizes as $size )
-            <tr>
+    {{-- @foreach ($stocks as $stock) --}}
+    @foreach ($sepatusizes as $stock)
+    {{-- @foreach ($sepatu->sizes as $size ) --}}
+    <tr>
                 <td>{{ $sepatusizes->firstItem() + $loop->index }}</td>
-                <td>{{ $sepatu->kode_sepatu }}</td>
-                <td>{{ $sepatu->nama }}</td>
-                <td>{{ $size->size }}</td>
-                <td>{{ $size->pivot->quantity }}</td>   
+                <td>{{ $stock->sepatus->kode_sepatu }}</td>
+                <td>{{ $stock->sepatus->nama }}</td>
+                <td>{{ $stock->sizes->size }}</td>
+                <td>{{ $stock->quantity }}</td>
                 <td class="text-nowrap">
-                    <a href="/dashboard-stock/{{ $sepatu->id }}/edit" class="btn btn-warning btn-sm" title="Edit"><i class="bi bi-pencil-square"></i></a>
+                    <a href="/dashboard-stock/{{ $stock->id }}/edit" class="btn btn-warning btn-sm" title="Edit"><i class="bi bi-pencil-square"></i></a>
 
-                    {{-- <form action="/dashboard-stock/{{ $sepatusizes->id }}" method="post" class="d-inline">
+                    <form action="/dashboard-stock/{{ $stock->id }}" method="post" class="d-inline">
                         @method('DELETE')
                         @csrf
                         <button title="Delete" class="btn btn-danger btn-sm" onclick="return confirm('Apakah anda ingin menghapus pemasukan ini?')">
                             <i class="bi bi-trash"></i>
                         </button>
-                    </form> --}}
+                    </form>
                 </td>
             </tr>
-        @endforeach
+            {{-- @endforeach --}}
+        {{-- @endforeach --}}
     @endforeach
 </table>
 {{ $sepatusizes->links() }}
